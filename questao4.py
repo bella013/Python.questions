02a15a15025ae409219b19ebf52234ef6e2768bd
Izabella Jorge <elyeiza@hotmail.com>
2020-08-24 19:22:43 -0300

    nova questão

---
+++ questao4.py
@@ -0,0 +1,7 @@
+print("Informe abaixo suas notas bimestrais")
+nota1 = float(input("Primeira nota: "))
+nota2 = float(input("Segunda nota: "))
+nota3 = float(input("Terceira nota: "))
+nota4 = float(input("Quarta nota: "))
+media = (nota1+nota2+nota3+nota4)/4
+print("Sua média é: ", media)
